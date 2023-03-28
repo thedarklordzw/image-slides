2ba3c5e25be89b421db6b8b8bb7e0ee6b6751dd9
Ulathi Ncube <swiftly.graphics@gmail.com>
2023-03-29 01:32:54 +0200

    project-2 finish

--- script.js
+++ script.js
@@ -6,18 +6,32 @@ const circles = document.querySelectorAll('.circle');
 let currentActive = 1;
 
 const update = () => {
+  if (currentActive > 1) {
+    prevBtn.disabled = false;
+  }
+
+  if (currentActive === 1) {
+    prevBtn.disabled = true;
+    nextBtn.disabled = false;
+  }
+
+  if (currentActive === circles.length) {
+    nextBtn.disabled = true;
+  }
+
   circles.forEach((circle, index) => {
     // circle.classList.remove('active');
 
     if (index < currentActive) {
       circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
     }
   });
 
   const actives = document.querySelectorAll('.active');
-  console.log(actives.length, circles.length);
 
-  const fraction = (actives.length / circles.length) * 100;
+  const fraction = ((actives.length - 1) / (circles.length - 1)) * 100;
   progress.style.width = fraction + '%';
 };
 
@@ -37,6 +51,8 @@ prevBtn.addEventListener('click', () => {
   if (currentActive < 1) {
     currentActive = 1;
   }
+
+  console.log(currentActive);
 
   update();
 });
